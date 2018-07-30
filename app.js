44b5c762f233069097408c4e7ffea9677e0d0eba
Baiang <973366510@qq.com>
2018-07-30 15:15:58 +0800

    update

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const Koa = require('koa');
 const app = new Koa();
 const static  = require('koa-static');
 const path = require('path')
+const config = require('./config/config.global');
+
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 // 配置静态资源
 const staticPath = './dist'
@@ -9,5 +12,6 @@ app.use(static(
     path.join( __dirname, staticPath)
 ))
 
-app.listen(3000);
-console.log('启动成功3000')      //日志打印
+app.listen(config[env].port, config[env].host, () => {
+  console.log(`启动成功${config[env].port}`)      //日志打印
+});
