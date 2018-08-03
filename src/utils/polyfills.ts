403c744aa1bbd1f79181af9e0b54805b1ded5a74
Baiang <973366510@qq.com>
2018-08-03 17:01:42 +0800

    修复set、map在ie9兼容性

--- src/utils/polyfills.ts
+++ src/utils/polyfills.ts
@@ -2,6 +2,9 @@
 // core-js comes with Next.js. So, you can import it like below
 import includes from 'core-js/library/fn/string/virtual/includes'
 import repeat from 'core-js/library/fn/string/virtual/repeat'
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
 
 // Add your polyfills
 // This files runs at the very beginning (even before React and Next.js core)
